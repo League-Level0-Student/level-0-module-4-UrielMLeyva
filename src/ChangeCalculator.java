9644ce34f877be9bfdf2fcf97c10e65b322d9fad
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-04-07 10:28:25 -0700

    Change Calculator Happy Pet

--- src/ChangeCalculator.java
+++ src/ChangeCalculator.java
@@ -13,18 +13,28 @@ public class ChangeCalculator {
 
 	public static void main(String[] args) {
 
-		// Ask the user how many nickels they have
+		// Ask the user how many nickels they have;
+		String n = JOptionPane.showInputDialog("How many nickels do you have?");
 
 		// Convert their answer to an int using Integer.parseInt()
-
+		int nickels = Integer.parseInt(n);
+	
 		// Ask the user how many dimes they have, and convert their answer
-
-		// Ask the user how many quarters they have, and convert their answer
-
+		String d = JOptionPane.showInputDialog("How many dimes do you have?");
+		int dimes = Integer.parseInt(d);
+		
+		//Ask user how many quarters they have, and convert their answer
+		String q = JOptionPane.showInputDialog("How many quarters do you have?");
+		int quarters = Integer.parseInt(q);
 		// Calculate how much money the user has and save it in a double variable 
-
+		double money = nickels*.05 + dimes*.10 + quarters*.25;
+		
+		
+		
+		
+		
 		// Tell the user how much money they have
-
+		 JOptionPane.showMessageDialog(null, "You have "+money+" in your pocket");
 	}
 }
 
