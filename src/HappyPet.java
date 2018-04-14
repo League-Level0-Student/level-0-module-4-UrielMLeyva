0299f48e861cb3dbc044dd770185fbea679b8eaf
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-04-14 10:53:59 -0700

    Happy Pet

--- src/HappyPet.java
+++ src/HappyPet.java
@@ -10,21 +10,52 @@ public class HappyPet {
 		// 1. Ask the user what kind of pet they want to buy, and store their answer in a variable
 		String p = JOptionPane.showInputDialog("What kind of pet do you want?");
 		// 7. REPEAT steps 3 - 6 enough times to make your pet happy!
-		
+		for(int i= 0;i <=5; i++);
 			// 3. Use showOptionDialog to ask the user what they want to do to make their pet happy
 			//    (eg: cuddle, food, water, take a walk, groom, clean up poop).
 			//    Make sure to customize the title and question too.
-			int task = JOptionPane.showOptionDialog(null, "Question", "Title", 0, JOptionPane.INFORMATION_MESSAGE, null,
-					new String[] { "Button1", "Button2", "Button3" }, null);
-
+			int task = JOptionPane.showOptionDialog(null, "What do you want to do to make your pet happy?", "Happy Pet", 0, JOptionPane.INFORMATION_MESSAGE, null,
+							new String[] { "Feed it", "Give them a treat", "Play fetch" }, null);
+		
 			// 5. Use user input to call the appropriate method created in step 4.
-
+			if(task==0) {
+				FeedIt();
+				}
+					if(task==1 ) {
+						Treat();
+					}
+					
+					if(task==0) {
+						PlayFetch();
+					}
+			
 			// 6. If you determine the happiness level is large enough, tell the
 			//    user that he loves his pet and use break; to exit for loop.
-
-	}
+				if(happinessLevel<=5) {
+					JOptionPane.showMessageDialog(null, "You have made your pet happy!");
+				}
+				
+				
+				
+			}
 
 	// 4. Create methods to handle each of your user selections.
 	//    Each method should create a pop-up with the pet's response (eg. cat might purr when pet), 
 	//    and INCREMENT the pet's happiness Level.
+		
+		
+		
+		static void FeedIt() {
+			JOptionPane.showMessageDialog(null, "Feed it");
+			happinessLevel++; 
+		}
+		static void Treat() {
+			JOptionPane.showMessageDialog(null, "Give it a treat");
+			happinessLevel++; 
+		}
+		static void PlayFetch() {
+			JOptionPane.showMessageDialog(null, "Play fetch");
+			happinessLevel++; 
+		}
 }
+		
