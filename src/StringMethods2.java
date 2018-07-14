8cad7730024d59660f8a04b07f4c769e65ca169e
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-07-14 10:25:42 -0700

    String Methods 2

--- src/StringMethods2.java
+++ src/StringMethods2.java
@@ -7,19 +7,27 @@ public class StringMethods2 {
 		
 		// 1. Create a String variable and initialize it with
 		//    random upper and lower case characters.
-		String water = "";
-		
+		String water = "uhOJbswlbmdKROLPCNSjejDNDUekfagurgn";
 		// 2. Print your String to the console in upper case.
-		System.out.println();
+		System.out.println(water.toUpperCase());
+		
 		// 3. Print your String to the console in lower case.
-		System.out.println();
+		System.out.println(water.toLowerCase());
+		
 		// 4. Print the first 3 char's of your String
 		//    HINT: .substring(start,end)
+		System.out.println(water.substring(0, 3));
 		
 		// 5. Print a single char somewhere in the middle of your String
 		//    REMINDER: char's in string start at index 0
+		System.out.println(water.charAt(18));
 		
 		// 6. BONUS -- print the LAST 3 char's of your string using
 		//        .length() to determine WHERE the last 3 char's are located.
+		System.out.println(water.length());
+		System.out.println(water.substring(32,  35));
+		
+	
+	
 	}
 }
